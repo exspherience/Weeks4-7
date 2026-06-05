749b3a9402e793c9e876cddccf0e1480a5656fc2
LAPTOP-UENLTB1C\Diana <coding@ddinis.ca>
2026-06-05 17:15:14 -0400

    Completed Information exercise.

---
+++ Assets/Scripts/DescribeImage.cs
@@ -0,0 +1,40 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DescribeImage : MonoBehaviour
+{
+    public TextMeshProUGUI imageDescription;
+    float distanceThreshold = 1f;
+    public bool isBurger;
+    public bool isPizza;
+    public bool isIceCream;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        if (Vector2.Distance(transform.position, mousePos) < distanceThreshold)
+        {
+            if (isBurger)
+            {
+                imageDescription.text = "This is a hamburger.";
+            }
+            else if (isPizza)
+            {
+                imageDescription.text = "This is a slice of pizza.";
+            }
+            else if (isIceCream)
+            {
+                imageDescription.text = "This is an ice cream cone.";
+            }
+        }
+    }
+
+}
