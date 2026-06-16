63f8fac5b0221cfb29b926cacc55cbfc363ca897
LAPTOP-UENLTB1C\Diana <coding@ddinis.ca>
2026-06-16 16:48:24 -0400

    Week7 In class demo

---
+++ Assets/Scripts/Collections_Demo.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collections_Demo : MonoBehaviour
+{
+    public List<string> animals;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animals.Add("Raccoon");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
