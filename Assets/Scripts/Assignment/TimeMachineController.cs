46a2a4e5da8fb2c186de9673f78fd8db9f8171cb
LAPTOP-UENLTB1C\Diana <coding@ddinis.ca>
2026-07-05 15:23:02 -0400

    Added rotation logic for Machine Core

--- Assets/Scripts/Assignment/TimeMachineController.cs
+++ Assets/Scripts/Assignment/TimeMachineController.cs
@@ -7,6 +7,9 @@ public class TimeMachineController : MonoBehaviour
 {
     public TextMeshProUGUI yearText;
     public Slider yearSlider;
+    public GameObject timeMachineCore;
+    public float speed;
+    public float currentYear = 2010;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,11 +20,30 @@ public class TimeMachineController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // set speed based on distance of slider value from set current year
+        speed = currentYear - yearSlider.value;
         setYearText();
+        rotateCore();
     }
 
+    // Sets text for Year Display based on what value of slide is
     void setYearText()
     {
         yearText.text = "TARGET: " + yearSlider.value;
+    }
+
+    // Rotates the gear that is in the center of the time machine sprite
+    void rotateCore()
+    {
+        Vector3 currentRotation = timeMachineCore.transform.eulerAngles;
+        currentRotation.z += speed * Time.deltaTime;
+        timeMachineCore.transform.eulerAngles = currentRotation;
+    }
+
+    // Sets currentYear to value from slider
+    // Called when Time Travel button is Clicked
+    public void setCurrentYear()
+    {
+        currentYear = yearSlider.value;
     }
 }
