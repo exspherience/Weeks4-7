deaf3f82ab8806362cfafe577aa666a8482ec21d
LAPTOP-UENLTB1C\Diana <coding@ddinis.ca>
2026-07-04 22:38:47 -0400

    Set Up Canvas Elements. Set Text to display Slider Value.

--- Assets/Scripts/Assignment/TimeMachineController.cs
+++ Assets/Scripts/Assignment/TimeMachineController.cs
@@ -1,7 +1,12 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeMachineController : MonoBehaviour
 {
+    public TextMeshProUGUI yearText;
+    public Slider yearSlider;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +16,11 @@ public class TimeMachineController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        setYearText();
+    }
+
+    void setYearText()
+    {
+        yearText.text = yearSlider.value.ToString();
     }
 }
