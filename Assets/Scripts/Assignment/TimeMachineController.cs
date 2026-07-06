f4f009aff125b6744ee597fe426548b7481e34da
LAPTOP-UENLTB1C\Diana <coding@ddinis.ca>
2026-07-05 23:07:06 -0400

    Implemented light beam animation when button pressed

--- Assets/Scripts/Assignment/TimeMachineController.cs
+++ Assets/Scripts/Assignment/TimeMachineController.cs
@@ -8,8 +8,16 @@ public class TimeMachineController : MonoBehaviour
     public TextMeshProUGUI yearText;
     public Slider yearSlider;
     public GameObject timeMachineCore;
+    public GameObject lightBeam;
+
+    public bool timeTravelStart;
+    public float duration = 3f;
     public float speed;
     public float currentYear = 2010;
+    float beamProgress = 0f;
+
+    public Vector3 startValue;
+    public Vector3 endValue;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,6 +32,12 @@ public class TimeMachineController : MonoBehaviour
         speed = currentYear - yearSlider.value;
         setYearText();
         rotateCore();
+
+        if(timeTravelStart)
+        {
+            beamProgress += Time.deltaTime;
+            animateBeam();
+        }
     }
 
     // Sets text for Year Display based on what value of slide is
@@ -45,5 +59,39 @@ public class TimeMachineController : MonoBehaviour
     public void setCurrentYear()
     {
         currentYear = yearSlider.value;
+    }
+
+    public void startTimeTravel()
+    {
+        timeTravelStart = true;
+    }
+
+    void animateBeam()
+    {
+        // Lerp the beam scale so it grows
+        lightBeam.transform.localScale = Vector3.Lerp(startValue, endValue, beamProgress/duration);
+
+        // if the beam has grown to full size, swap the start and end so it shrinks down
+        if (beamProgress >= duration && lightBeam.transform.localScale.x != 0f)
+        {
+            setBeamSize(startValue, endValue);
+            beamProgress = 0f;
+        }
+        // if the beam hits width hits zero again, swap start and end so it can grow next time the button is pressed
+        // return timeTravelStart to false so that the animation can play again next button press
+        if(lightBeam.transform.localScale.x == 0f && beamProgress >= duration)
+        {
+            setBeamSize(startValue, endValue);
+            beamProgress = 0f;
+            timeTravelStart = false;
+        }
+    }
+
+    // swap the start and end sizes of the beam when called
+    void setBeamSize(Vector3 startSize, Vector3 endSize)
+    {
+        Vector3 tempValue = endValue;
+        endValue = startValue;
+        startValue = tempValue;
     }
 }
